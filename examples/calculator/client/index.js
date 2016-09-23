14f4d60dfb7ea50e777a279bec3f3fa63a415cff
Bartosz Czerwonka <czerwonka.bartosz@gmail.com>
2016-09-23 16:40:14 +0200

    Fixed example

--- examples/calculator/client/index.js
+++ examples/calculator/client/index.js
@@ -7,12 +7,14 @@ const microscopic = new Microscopic({
   }
 })
 
-const calculatorServiceClient = microscopic.createClient('calculator-service')
+setTimeout(() => {
+  const calculatorServiceClient = microscopic.createClient('calculator-service')
 
-calculatorServiceClient.send('add', { params: { a: 1, b: 2 } }, (error, response) => {
-  console.log(`ADD: ${response.result}`)
-})
+  calculatorServiceClient.send('add', { params: { a: 1, b: 2 } }, (error, response) => {
+    console.log(`ADD: ${response.result}`)
+  })
 
-calculatorServiceClient.send('subtract', { params: { a: 1, b: 2 } }, (error, response) => {
-  console.log(`SUBTRACT: ${response.result}`)
-})
+  calculatorServiceClient.send('subtract', { params: { a: 1, b: 2 } }, (error, response) => {
+    console.log(`SUBTRACT: ${response.result}`)
+  })
+}, 6 * 1000)
